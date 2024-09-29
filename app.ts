8d3ae5d2fc33c5f80cf0fa6c48b1007a82a699ca
Béla Jash <abelxotessema@gmail.com>
2024-09-29 14:18:25 +0300

    Connect Existing Relationships.

--- app.ts
+++ app.ts
@@ -3,26 +3,27 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // const user = await prisma.user.update({
-  //   where: {email: 'belajash@gmail.com'},
-  //   data: {email: 'belajash1@gmail.com'},
-  //   // include, select
+  // const preference = await prisma.userPreference.create({
+  //   data: {emailUpdates: true}
   // });
-  // const users = await prisma.user.updateMany({
-  //   where: {age: 22},
-  //   data: {age: 23},
-  //   // no include and update
-  // });
-  const user = await prisma.user.update({
-    where: {email: 'belajash1@gmail.com'},
-    // data: {age: {increment: 1}},
-    data: {age: {decrement: 1}},
-    // include, select
-  });
   
-  console.log(user);
-  // console.log(users);
-  // console.log(await prisma.user.findMany());
+  // const user = await prisma.user.update({
+  //   where: {email: 'belajash1@gmail.com'},
+  //   data: {
+  //     // userPreference: {connect: {id: 2}}
+  //     userPreference: {disconnect: true},
+  //   }
+  // });
+  
+  // const user = await prisma.user.create({
+  //   data: {
+  //     // name, age, email
+  //     userPreference: {connect: {id: 1}},
+  //   }
+  // });
+  
+  // console.log(preference);
+  // console.log(user);
 }
 
 main()
