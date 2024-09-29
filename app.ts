622f56a3f32d47741b779417684c5456da817591
Béla Jash <abelxotessema@gmail.com>
2024-09-29 13:47:25 +0300

    Relationship Filtering.

--- app.ts
+++ app.ts
@@ -5,31 +5,25 @@ const prisma = new PrismaClient();
 async function main() {
   const user = await prisma.user.findMany({
     where: {
-      // name: {equals: 'Bela Jash'},
-      // name: {not: 'Bela Jash'},
-      // name: {in: ['Bela Jash', 'Abel Tessema']},
-      // name: {notIn: ['Bela Jash', 'Abel Tessema']},
-      // age: {lt: 25}, // gt, gte, lte
-      // name: 'Bela Jash',
-      // email: {contains: '@gmail.com'},
-      // email: {endsWith: '@gmail.com'},
-      // email: {startsWith: 'a'},
-      // AND: [
-      //   // {email: {endsWith: '.com'}},
-      //   // {name: {startsWith: 'b'}},
-      //   {email: {startsWith: 'a'}},
-      //   {email: {endsWith: '.com'}},
-      // ]
-      // OR: [
-      //   {email: {startsWith: 'abel'}},
-      //   {age: {gt: 10}},
-      // ],
-      // NOT: [],
-      NOT: {email: {startsWith: 'abel'}},
+      // userPreference: {
+      //   // emailUpdates: true,
+      //   emailUpdates: false,
+      // }
+      writtenPosts: {
+        every: {title: 'Test'} // none, some
+      }
     },
   });
+  const post = await prisma.post.findMany({
+    where: {
+      author: { // isNot
+        is: {age: 22}
+      }
+    }
+  });
   
-  console.log(user);
+  // console.log(user);
+  console.log(post);
 }
 
 main()
