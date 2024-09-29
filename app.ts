94a60dd68fbf335d151f53d696714387c3432ca2
Béla Jash <abelxotessema@gmail.com>
2024-09-29 11:42:41 +0300

    Advanced Filtering.

--- app.ts
+++ app.ts
@@ -3,26 +3,30 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  // const user = await prisma.user.findUnique({
-    // where: {email: 'belajash@gmail.com'}
-    // We can optionally include or select specific fields here.
-    // where: {name_age: {name: 'Bela Jash', age: 22}} // name_age is a composite unique key.
-    // where: {name_age: {name: 'Bela Jash', age: 23}}
-    // where: {name: 'Bela Jash'} // Not allowed because name isn't unique.
-  // });
-  
-  // const user = await prisma.user.findFirst({
-  //   where: {name: 'Bela Jash'}
-  // });
-  
   const user = await prisma.user.findMany({
-    where: {name: 'Bela Jash'},
-    // distinct: 'name'
-    distinct: ['name', 'age'],
-    take: 2,
-    skip: 1,
-    // orderBy: {age: 'asc'},
-    orderBy: [{age: 'asc'}, {name: 'desc'}],
+    where: {
+      // name: {equals: 'Bela Jash'},
+      // name: {not: 'Bela Jash'},
+      // name: {in: ['Bela Jash', 'Abel Tessema']},
+      // name: {notIn: ['Bela Jash', 'Abel Tessema']},
+      // age: {lt: 25}, // gt, gte, lte
+      // name: 'Bela Jash',
+      // email: {contains: '@gmail.com'},
+      // email: {endsWith: '@gmail.com'},
+      // email: {startsWith: 'a'},
+      // AND: [
+      //   // {email: {endsWith: '.com'}},
+      //   // {name: {startsWith: 'b'}},
+      //   {email: {startsWith: 'a'}},
+      //   {email: {endsWith: '.com'}},
+      // ]
+      // OR: [
+      //   {email: {startsWith: 'abel'}},
+      //   {age: {gt: 10}},
+      // ],
+      // NOT: [],
+      NOT: {email: {startsWith: 'abel'}},
+    },
   });
   
   console.log(user);
