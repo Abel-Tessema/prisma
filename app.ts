d5e7c0cb934aae7f43196efccf5a0f832f40332d
Béla Jash <abelxotessema@gmail.com>
2024-09-29 14:02:24 +0300

    Update records.

--- app.ts
+++ app.ts
@@ -3,27 +3,26 @@ import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.findMany({
-    where: {
-      // userPreference: {
-      //   // emailUpdates: true,
-      //   emailUpdates: false,
-      // }
-      writtenPosts: {
-        every: {title: 'Test'} // none, some
-      }
-    },
-  });
-  const post = await prisma.post.findMany({
-    where: {
-      author: { // isNot
-        is: {age: 22}
-      }
-    }
+  // const user = await prisma.user.update({
+  //   where: {email: 'belajash@gmail.com'},
+  //   data: {email: 'belajash1@gmail.com'},
+  //   // include, select
+  // });
+  // const users = await prisma.user.updateMany({
+  //   where: {age: 22},
+  //   data: {age: 23},
+  //   // no include and update
+  // });
+  const user = await prisma.user.update({
+    where: {email: 'belajash1@gmail.com'},
+    // data: {age: {increment: 1}},
+    data: {age: {decrement: 1}},
+    // include, select
   });
   
-  // console.log(user);
-  console.log(post);
+  console.log(user);
+  // console.log(users);
+  // console.log(await prisma.user.findMany());
 }
 
 main()
